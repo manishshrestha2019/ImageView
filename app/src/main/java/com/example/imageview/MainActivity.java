49b644f40f1afd637737c8f7988a09c27185a86e
Manish Shrestha <manish.sh2071@gmail.com>
2019-04-01 10:58:00 +0545

    added click listner and use switch

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,19 +2,44 @@ package com.example.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private RadioButton rd1,rd2,rd3;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private RadioButton rdArya,rdJon,rdTyrion;
+    private TextView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rd1=findViewById(R.id.rd1);
-        rd2=findViewById(R.id.rd2);
-        rd3=findViewById(R.id.rd3);
+        rdArya=findViewById(R.id.rd1);
+        rdJon=findViewById(R.id.rd2);
+        rdTyrion=findViewById(R.id.rd3);
+        ivImage.findViewById(R.id.ivImage);
+
+        rdArya.setOnClickListener(this);
+        rdJon.setOnClickListener(this);
+        rdTyrion.setOnClickListener(this);
+
+
+
 
     }
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.rd1:
+                break;
+            case R.id.rd2:
+                break;
+
+            case R.id.rd3:
+                break;
+        }
+    }
 }
+
