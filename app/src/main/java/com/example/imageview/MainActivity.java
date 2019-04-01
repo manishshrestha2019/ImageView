7e83b55072353bf844a35dba6fecfc82c6ed9eb7
Manish Shrestha <manish.sh2071@gmail.com>
2019-04-01 10:43:09 +0545

    added Image

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+    private RadioButton rd1,rd2,rd3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rd1=findViewById(R.id.rd1);
+        rd2=findViewById(R.id.rd2);
+        rd3=findViewById(R.id.rd3);
+
     }
 }
